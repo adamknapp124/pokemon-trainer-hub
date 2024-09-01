98cf836506353373e7f885e7efa7288181729e45
Adam Knapp <aknapp124@gmail.com>
2024-09-01 11:43:34 -0700

    Made logo in navbar link to home and added routes for navigation

--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Link from 'next/link';
+
 import Image from 'next/image';
 
 import { Navlinks, authLinks } from '@/app/constants/navLinks';
@@ -12,13 +14,15 @@ export default function Navbar() {
 	return (
 		<nav className='bg-black h-[100px] relative'>
 			<section className='justify-center h-full items-center flex justify-between max-w-[1280px] m-auto font-navbar text-xl font-bold tracking-wide'>
-				<Image
-					src={logo}
-					alt='logo'
-					width={175}
-					height={175}
-					className='cursor-pointer'
-				/>
+				<Link href='/'>
+					<Image
+						src={logo}
+						alt='logo'
+						width={175}
+						height={175}
+						className='cursor-pointer'
+					/>
+				</Link>
 				<div className='flex gap-10 text-white'>
 					{Navlinks.map((link) => (
 						<NavLink
