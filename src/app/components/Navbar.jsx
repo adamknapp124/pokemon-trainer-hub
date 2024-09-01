ca18b11d79bac4f90d17a47e9380351e0121c2f4
Adam Knapp <aknapp124@gmail.com>
2024-08-31 23:56:37 -0700

    Initally styled navbar, added comments for next steps

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -2,48 +2,20 @@ import React from 'react';
 
 import Image from 'next/image';
 
+import { Navlinks, authLinks } from '@/app/constants/navLinks';
+
 import logo from '../../../public/images/poke-logo.png';
 import Divider from '../components/Divider';
-
-const Navlinks = [
-	{
-		name: 'Features',
-		link: '/features',
-	},
-	{
-		name: 'Updates',
-		link: '/updates',
-	},
-	{
-		name: 'Requests',
-		link: '/requests',
-	},
-	{
-		name: 'Guide',
-		link: '/guide',
-	},
-];
-
-const authLinks = [
-	{
-		name: 'Login',
-		link: '/login',
-	},
-	{
-		name: 'Sign up',
-		link: '/signup',
-	},
-];
 
 export default function Navbar() {
 	return (
 		<nav className='bg-black h-[100px] relative'>
-			<section className='justify-center h-full items-center flex justify-between max-w-[1280px] m-auto font-navbar text-xl'>
+			<section className='justify-center h-full items-center flex justify-between max-w-[1280px] m-auto font-navbar text-xl font-bold tracking-wide'>
 				<Image
 					src={logo}
 					alt='logo'
-					width={150}
-					height={150}
+					width={175}
+					height={175}
 					className='cursor-pointer'
 				/>
 				<div className='flex gap-10 text-white'>
@@ -61,3 +33,6 @@ export default function Navbar() {
 		</nav>
 	);
 }
+
+// Create navlink component to break down code even more, then use the map function to replace the buttons with nav
+// Links for navigation
---
+++ src/app/constants/navLinks.js
@@ -0,0 +1,31 @@
+const Navlinks = [
+	{
+		name: 'Features',
+		link: '/features',
+	},
+	{
+		name: 'Updates',
+		link: '/updates',
+	},
+	{
+		name: 'Requests',
+		link: '/requests',
+	},
+	{
+		name: 'Guide',
+		link: '/guide',
+	},
+];
+
+const authLinks = [
+	{
+		name: 'Login',
+		link: '/login',
+	},
+	{
+		name: 'Sign up',
+		link: '/signup',
+	},
+];
+
+export { Navlinks, authLinks };
